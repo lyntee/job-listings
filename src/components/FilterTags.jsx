30aea8ad9ec766c22a1c932d5103ad61babe79cb
lyntee <lynleen54@gmail.com>
2024-03-15 07:11:56 +0800

    filter tags active states

--- src/components/FilterTags.jsx
+++ src/components/FilterTags.jsx
@@ -34,6 +34,18 @@ const FilterTags = () => {
               backgroundColor: "hsl(180, 29%, 50%)",
               color: "#fafafa",
             }}
+            onMouseMove={(e) => {
+              e.currentTarget.style.backgroundColor = "hsl(180, 14%, 20%)";
+              e.currentTarget.parentNode.style.backgroundColor =
+                "hsl(180, 14%, 20%)";
+              e.currentTarget.style.cursor = "pointer";
+            }}
+            onMouseOut={(e) => {
+              e.currentTarget.style.backgroundColor = "hsl(180, 29%, 50%)";
+              e.currentTarget.parentNode.style.backgroundColor =
+                "hsl(180, 29%, 50%)";
+              e.currentTarget.style.cursor = "pointer";
+            }}
           />
         </div>
       ))}
