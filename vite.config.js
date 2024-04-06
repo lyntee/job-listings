3347811dcbcc3b912a791e060208f752720c341a
lyntee <lynleen54@gmail.com>
2024-04-06 13:38:46 +0800

    update static images folder path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/job-listings/",
   plugins: [react()],
-  // base: "/job-listings/",
-  base: "/",
   build: {
     outDir: "dist",
   },
