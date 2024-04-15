b81545a3a39a55f1901661e29ea23b0572fb2394
lyntee <lynleen54@gmail.com>
2024-04-16 07:25:36 +0800

    header component routing for deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 const Header = ({ view }) => {
   return (
     <header>
-      <img src={`../images/bg-header-${view}.svg`} alt="bg-header" />
+      {/* <img src={`../images/bg-header-${view}.svg`} alt="bg-header" /> */}
+      {/* add this line only for deployment */}
+      <img src={`./images/bg-header-${view}.svg`} alt="bg-header" />
     </header>
   );
 };
