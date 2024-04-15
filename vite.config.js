0bbadb9deeb7a118c19687bdc9fe9cd388a88c5b
lyntee <lynleen54@gmail.com>
2024-04-16 07:19:04 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // add this line only for deployment
-  // base: "/job-listings",
+  base: "/job-listings",
   plugins: [react()],
   build: {
     outDir: "dist",
